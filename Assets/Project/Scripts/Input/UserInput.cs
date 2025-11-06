6422491a3424c9f69e4aadae57c13952d7b260f7
Mikita WhaleTee Durunda <n.rob.durunda@gmail.com>
2025-11-06 14:10:55 +0300

    add TMP, DOTween, UniTask, Singleton,

---
+++ Assets/Project/Scripts/Input/UserInput.cs
@@ -0,0 +1,25 @@
+﻿using Project.Scripts.Misc;
+using UnityEngine;
+
+namespace Project.Scripts.Input
+{
+    public class UserInput : Singleton<UserInput>
+    {
+        private readonly InputActions inputActions = new InputActions();
+
+        public Vector2 Movement => inputActions.Player.Move.ReadValue<Vector2>();
+        public bool JumpPerformed => inputActions.Player.Jump.IsPressed() && inputActions.Player.Jump.WasPressedThisFrame();
+        public bool AttackPerformed => inputActions.Player.Attack.IsPressed() && inputActions.Player.Attack.WasPressedThisFrame();
+        public bool AttackPressed => inputActions.Player.Attack.IsPressed();
+
+        private void OnEnable()
+        {
+            inputActions.Enable();
+        }
+
+        private void OnDisable()
+        {
+            inputActions.Disable();
+        }
+    }
+}
---
+++ Assets/Project/Scripts/Misc/Singleton.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+namespace Project.Scripts.Misc
+{
+    public class Singleton<T> : MonoBehaviour where T : Component
+    {
+        private static T instance;
+
+        public static bool HasInstance => instance != null;
+        public static void TryGetInstance(out T i) => i = HasInstance ? instance : null;
+
+        public static T Instance
+        {
+            get
+            {
+                if (instance != null) return instance;
+                instance = FindAnyObjectByType<T>();
+                if (instance != null) return instance;
+                var go = new GameObject(typeof(T).Name + " Generated");
+                instance = go.AddComponent<T>();
+                return instance;
+            }
+        }
+
+        protected virtual void Awake() => InitializeSingleton();
+
+        protected virtual void InitializeSingleton()
+        {
+            if (!Application.isPlaying) return;
+
+            if (instance == null)
+            {
+                instance = this as T;
+                DontDestroyOnLoad(gameObject);
+            }
+            else
+            {
+                if (instance != this)
+                {
+                    Destroy(gameObject);
+                }
+            }
+        }
+    }
+}
