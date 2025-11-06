b3cdd00efe9895b7e91f2e4d1ebe1cbb5d5ce154
CryptoSolve <lion.si@yandex.ru>
2025-11-06 23:36:25 +0300

    add DI Reflex basis
    add DI Reflex basis

---
+++ Assets/Project/Scripts/DI/ProjectInstaller.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using Reflex.Core;
+using Project.Scripts.Input;
+
+namespace DI
+{
+    public class ProjectInstaller : MonoBehaviour, IInstaller
+    {
+        private const string ROOT_CONTAINER_NAME = "Root Container";
+
+        private Container rootContainer;
+
+        private static void InstallMessagePipe(ContainerBuilder rootContainerBuilder)
+        {
+        }
+
+        private static void InstallServices(ContainerBuilder containerBuilder)
+        {
+            containerBuilder.AddSingleton(typeof(UserInput));
+        }
+
+        public void InstallBindings(ContainerBuilder containerBuilder)
+        {
+            containerBuilder.SetName(ROOT_CONTAINER_NAME);
+            InstallMessagePipe(containerBuilder);
+            InstallServices(containerBuilder);
+        }
+    }
+}
