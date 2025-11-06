5fd47f76ad8b80544e7dfd5d377cef4bad2f2ba9
Mikita WhaleTee Durunda <n.rob.durunda@gmail.com>
2025-11-06 16:06:23 +0300

    update UserInput

--- Assets/Project/Scripts/Input/UserInput.cs
+++ Assets/Project/Scripts/Input/UserInput.cs
@@ -5,21 +5,36 @@ namespace Project.Scripts.Input
 {
     public class UserInput : Singleton<UserInput>
     {
-        private readonly InputActions inputActions = new InputActions();
+        private InputActions inputActions;
 
         public Vector2 Movement => inputActions.Player.Move.ReadValue<Vector2>();
         public bool JumpPerformed => inputActions.Player.Jump.IsPressed() && inputActions.Player.Jump.WasPressedThisFrame();
+        public bool JumpPressed => inputActions.Player.Jump.IsPressed();
         public bool AttackPerformed => inputActions.Player.Attack.IsPressed() && inputActions.Player.Attack.WasPressedThisFrame();
         public bool AttackPressed => inputActions.Player.Attack.IsPressed();
 
-        private void OnEnable()
+        private void OnEnable() => ActivateInputActions();
+
+        private void Start()
         {
+            DeactivateInputActions();
+            ActivateInputActions();
+        }
+
+        private void OnDisable() => DeactivateInputActions();
+
+        private void ActivateInputActions()
+        {
+            inputActions = new InputActions();
             inputActions.Enable();
         }
 
-        private void OnDisable()
+        private void DeactivateInputActions()
         {
-            inputActions.Disable();
+            inputActions?.Player.Disable();
+            inputActions?.UI.Disable();
+            inputActions?.Disable();
+            inputActions?.Dispose();
         }
     }
 }
