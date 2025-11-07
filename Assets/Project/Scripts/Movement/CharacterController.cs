38addefbf4e85459b8795699c76a5c5dc0baf754
Mikita WhaleTee Durunda <n.rob.durunda@gmail.com>
2025-11-07 13:17:32 +0300

    add movement and jump

---
+++ Assets/Project/Scripts/Movement/CharacterController.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+namespace Project.Scripts.Movement
+{
+    [RequireComponent(typeof(CharacterMovement))]
+    [RequireComponent(typeof(CharacterJump))]
+    public class CharacterController : MonoBehaviour
+    {
+        [SerializeField] private PresetObject preset;
+
+        private CharacterMovement moveScript;
+        private CharacterJump jumpScript;
+
+        private PresetObject installedPreset;
+
+        private void Awake()
+        {
+            moveScript = GetComponent<CharacterMovement>();
+            jumpScript = GetComponent<CharacterJump>();
+
+            InstallPresetData();
+        }
+
+        private void FixedUpdate()
+        {
+            if (installedPreset != preset) InstallPresetData();
+        }
+
+        private void InstallPresetData()
+        {
+            
+            moveScript.InstallPreset(preset);
+            jumpScript.InstallPreset(preset);
+
+            installedPreset = preset;
+        }
+    }
+}
