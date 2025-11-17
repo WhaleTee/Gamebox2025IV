2e18de10f69b8ffea2a7c049485c59ba6dfda29c
Mikita WhaleTee Durunda <n.rob.durunda@gmail.com>
2025-11-17 11:29:28 +0300

    add example damageable

---
+++ Assets/Project/Scripts/Combat/DestroyOnDeath.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace Combat
+{
+    [RequireComponent(typeof(DamageableObject))]
+    public class DestroyOnDeath : MonoBehaviour
+    {
+        private DamageableObject damageableObject;
+
+        private void Awake()
+        {
+            damageableObject = GetComponent<DamageableObject>();
+        }
+
+        private void Start()
+        {
+            damageableObject.OnDeath += OnDeath;
+        }
+
+        private void OnDeath()
+        {
+            Destroy(gameObject);
+        }
+    }
+}
