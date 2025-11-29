452595ae9105224f7856f6e8795a0e93be71f240
CryptoSolve <lion.si@yandex.ru>
2025-11-29 16:01:34 +0300

    fix ui diverge

--- Assets/Project/Scripts/Characters/ArtifactPickup.cs
+++ Assets/Project/Scripts/Characters/ArtifactPickup.cs
@@ -1,6 +1,7 @@
 ﻿using Characters;
 using Combat.Weapon;
 using Extensions;
+using LevelProgression;
 using UnityEngine;
 
 [RequireComponent(typeof(Collider2D))]
@@ -8,6 +9,7 @@ public class ArtifactPickup : MonoBehaviour
 {
     [SerializeField] private LayerMask layers;
     [SerializeField] private WeaponProjectiled artifact;
+    [SerializeField] private DamageModifier damageModifier;
 
     private Collider2D _collider;
 
@@ -20,12 +22,24 @@ public class ArtifactPickup : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D collider)
     {
         if (!layers.Contains(collider.gameObject.layer)) return;
+        var hero = collider.GetComponent<Hero>();
+        EquipWeapon(hero);
+        AddModifier(hero);
+        Destroy(gameObject);
+    }
+
+    private void AddModifier(Hero hero)
+    {
+        if (hero.TryGetComponent(out PlayerAbilities playerAbilities))
+            playerAbilities.AddModifier(damageModifier);
+    }
+
+    private void EquipWeapon(CharacterBase owner)
+    {
         var weapon = Instantiate(artifact);
-        var owner = collider.GetComponent<Hero>();
         weapon.Install(owner);
         weapon.Init();
         weapon.gameObject.SetActive(false);
         owner.Inventory.Add(weapon);
-        Destroy(gameObject);
     }
 }
